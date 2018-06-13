f717bc5d39bc840855ef8e80f4926c04e9343c80
TsutomuTakai <wesley.takai@gmail.com>
2018-06-13 16:25:37 -0300

    History and actual avaliation screens added

--- src/app/modules/evaluation/components/show-actual/show-actual.component.ts
+++ src/app/modules/evaluation/components/show-actual/show-actual.component.ts
@@ -6,7 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-actual.component.scss']
 })
 export class ShowActualComponent implements OnInit {
+    public people = [
+        {
+            name: 'Eduardo Castro',
+            job: 'Desenvolvedor Pleno',
+            picture: '../../../../../assets/img/8.jpg',
+            status: true
+        },
+        {
+            name: 'Gina Albuquerque',
+            job: 'Desenvolvedora Pleno',
+            picture: './../../../../assets/img/3.jpg',
+            status: true
 
+        },
+        {
+            name: 'Rafael Lider',
+            job: 'Desenvolvedor Junior',
+            picture: './../../../../assets/img/53.jpg',
+            status: false
+        },
+        {
+            name: 'Eduardo Castro',
+            job: 'Desenvolvedor Pleno',
+            picture: '../../../../../assets/img/8.jpg',
+            status: true
+        },
+        {
+            name: 'Gina Albuquerque',
+            job: 'Desenvolvedora Pleno',
+            picture: './../../../../assets/img/3.jpg',
+            status: true
+
+        },
+        {
+            name: 'Rafael Lider',
+            job: 'Desenvolvedor Junior',
+            picture: './../../../../assets/img/53.jpg',
+            status: false
+        },
+    ]
   constructor() { }
 
   ngOnInit() {
