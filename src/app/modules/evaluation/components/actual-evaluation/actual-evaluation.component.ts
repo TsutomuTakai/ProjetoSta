29c51d17c27e12d2f3253d060ade33545ec29434
TsutomuTakai <wesley.takai@gmail.com>
2018-06-15 15:14:18 -0300

    Icones de status de avaliação

--- src/app/modules/evaluation/components/actual-evaluation/actual-evaluation.component.ts
+++ src/app/modules/evaluation/components/actual-evaluation/actual-evaluation.component.ts
@@ -11,39 +11,45 @@ export class ActualEvaluationComponent implements OnInit {
             name: 'Eduardo Castro',
             job: 'Desenvolvedor Pleno',
             picture: '../../../../../assets/img/8.jpg',
-            status: true
+            status: true,
+            aval: false,
         },
         {
             name: 'Gina Albuquerque',
             job: 'Desenvolvedora Pleno',
             picture: './../../../../assets/img/3.jpg',
-            status: true
+            status: true,
+            aval: true,
 
         },
         {
             name: 'Rafael Lider',
             job: 'Desenvolvedor Junior',
             picture: './../../../../assets/img/53.jpg',
-            status: false
+            status: false,
+            aval: true,
         },
         {
             name: 'Eduardo Castro',
             job: 'Desenvolvedor Pleno',
             picture: '../../../../../assets/img/8.jpg',
-            status: true
+            status: true,
+            aval: false,
         },
         {
             name: 'Gina Albuquerque',
             job: 'Desenvolvedora Pleno',
             picture: './../../../../assets/img/3.jpg',
-            status: true
+            status: true,
+            aval: true,
 
         },
         {
             name: 'Rafael Lider',
             job: 'Desenvolvedor Junior',
             picture: './../../../../assets/img/53.jpg',
-            status: false
+            status: false,
+            aval: false,
         },
     ]
   constructor() { }
